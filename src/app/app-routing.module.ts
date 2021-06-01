acd29ef0bc83a599c8c07914b6e7a7aa56ec04d9
Fitonho <filipe.mouro@unesp.br>
2021-06-01 00:26:44 -0300

    adicionando rota para criar uma nova ação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
+import { CreateUpdateFormComponent } from './shared/forms/create-update-form/create-update-form.component';
 
 const routes: Routes = [
   { path: '', component: DashboardPageComponent },
+  { path: 'add', component: CreateUpdateFormComponent },
 ];
 
 @NgModule({
